6819064e2047886800d5034431590d387a36b9de
Hello!!! Eu sou Fabiana Cunha!!! <107757832+ProfFabiana@users.noreply.github.com>
2025-09-10 17:28:34 +0000

    ufa deu certo

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o Screen Match");
-                System.out.println("Filme: Top Gun: Maverick");
+            System.out.println("Esse é o Screen Match");
+            System.out.println("Filme: Top Gun: Maverick");
 
                 int anoDeLancamento = 2022;
-                System.out.println("Ano de lançamento:" + anoDeLancamento");
+                System.out.println("Ano de lançamento:" + anoDeLancamento);
                 boolean incluidoNoPlano = true;
                 double notaDoFilme = 8.1;
 
@@ -12,3 +12,4 @@ public class Main {
         System.out.println(media);
     }
 }
+    
